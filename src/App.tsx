be06ae419935accbc5d3d5ebcdd63b0db4c97815
princeVegeta2 <nellatorbeaufort@gmail.com>
2024-09-10 16:45:12 +0200

    Last commit 9/10/2024

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import './App.module.css';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 
 const App: React.FC = () => {
   return (
